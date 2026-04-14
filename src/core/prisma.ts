d814236655c024632ba8fc57ff75405b08600533
wizcoderr <ssayam2000@gmail.com>
2026-04-14 06:25:11 +0530

    migrations

--- src/core/prisma.ts
+++ src/core/prisma.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient()
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
